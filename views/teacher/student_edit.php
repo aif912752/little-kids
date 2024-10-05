8aeaf4c53623e1ec1995e08162b23ca90e405ffb
warunyoo1 <151112524+warunyoo1@users.noreply.github.com>
2024-10-05 14:04:43 +0700

    update edit

--- views/teacher/student_edit.php
+++ views/teacher/student_edit.php
@@ -30,114 +30,116 @@ if ($id) {
 
 
 <!DOCTYPE html>
-
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>เพิ่มข้อมูลนักเรียน</title>
+    <title>แก้ไขข้อมูลผู้ดูแลระบบ</title>
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
+    <style>
+        .swal2-confirm {
+            background-color: #2563EB !important;
+            color: white !important;
+        }
+    </style>
 </head>
 
-<body class=" bg-surface">
-<main>
-<div id="main-wrapper" class=" flex p-5 xl:pr-0">
+<body class=" font-inter">
+    <div class="flex h-screen bg-gray-50 " :class="{ 'overflow-hidden': isSideMenuOpen }">
 
         <?php include '../../src/navbar_teacher.php'; ?>
-
-        <div class=" w-full page-wrapper xl:px-6 px-0">
-
-        <main class="h-full  max-w-full">
-        <div class="w-full">
+        <div class="flex flex-col w-full overflow-y-auto">
+            <div class="w-full">
                 <div class="bg-gradient-to-b from-blue-500 to-blue-300 h-96"></div>
                 <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
                     <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
-                        <p class="text-3xl font-bold leading-7 text-center">เพิ่มข้อมูลนักเรียน</p>
-                        <form action="insert_student.php" method="post">
+                        <p class="text-3xl font-bold leading-7 text-center">แก้ไขข้อมูลนักเรียน</p>
+                        <form action="update_admin.php" method="post">
                             <div class="md:flex items-center mt-4">
                                 <div class="w-full md:w-1/2 flex flex-col">
-                                    <label class="font-semibold leading-none">ชื่อจริง</label>
-                                    <input type="text" name="first_name" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <label class="font-semibold leading-none">Username</label>
+                                    <input type="text" name="username" value="<?php echo $user['username']  ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <input type="hidden" name="id" value="<?php echo $id; ?>">
+                                    <input type="hidden" name="old_username" value="<?php echo $user['username']; ?>">
                                 </div>
-                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 ">
+                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0">
+                                    <label class="font-semibold leading-none"> Password </label>
+                                    <input type="password" name="password" value="<?php echo $user['password'];  ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                </div>
+                            </div>
+                            <div class="md:flex items-center mt-4">
+                                <div class="w-full md:w-1/2 flex flex-col">
+                                    <label class="font-semibold leading-none">ชื่อ</label>
+                                    <input type="text" name="first_name" value="<?= $row['first_name'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                </div>
+                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0">
                                     <label class="font-semibold leading-none">นามสกุล</label>
-                                    <input type="text" name="last_name" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <input type="text" name="last_name" value="<?= $row['last_name'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
                             </div>
                             <div class="md:flex items-center mt-4">
                                 <div class="w-full md:w-1/2 flex flex-col">
-                                    <label class="font-semibold leading-none">วันเดือนปีเกิด</label>
-                                    <input type="date" name="birthdate" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <label class="font-semibold leading-none"> เลขประจำตัวประชาชน </label>
+                                    <input type="text" name="citizen_id" value="<?= $row['citizen_id'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
-                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 ">
+                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0">
+                                    <label class="font-semibold leading-none">วัน/เดือน/ปี เกิด</label>
+                                    <input type="date" name="birthdate" value="<?= $row['birthdate'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                </div>
+                            </div>
+                            <div class="md:flex items-center mt-4">
+                                <div class="w-full md:w-1/2 flex flex-col">
                                     <label class="font-semibold leading-none">เชื้อชาติ</label>
-                                    <input type="text" name="ethnicity" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <input type="text" name="ethnicity" value="<?= $row['ethnicity'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
-                            </div>
-                            <div class="md:flex items-center mt-4">
-                                <div class="w-full md:w-1/2 flex flex-col">
+                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0">
                                     <label class="font-semibold leading-none">สัญชาติ</label>
-                                    <input type="text" name="nationality" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <input type="text" name="nationality" value="<?= $row['nationality'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
-                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 ">
+                            </div>
+                            <div class="md:flex items-center mt-4">
+                                <div class="w-full  flex flex-col">
                                     <label class="font-semibold leading-none">ศาสนา</label>
-                                    <input type="text" name="religion" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <input type="text" name="religion" value="<?= $row['religion'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
+
                             </div>
                             <div class="md:flex items-center mt-4">
-                                <div class="w-full md:w-1/2 flex flex-col">
-                                    <label class="font-semibold leading-none">เพศนักเรียน</label>
-                                    <input type="text" name="gender" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
-                                </div>
-                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 ">
-                                    <label class="font-semibold leading-none">รหัสบัตรประชาชน</label>
-                                    <input type="text" name="citizen_id" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
-                                </div>
-                            </div>
-                            <div class="md:flex items-center mt-4">
-                                <div class="w-full md:w-1/2 flex flex-col">
+                                <div class="w-full  flex flex-col">
                                     <label class="font-semibold leading-none">วันที่ลงทะเบียนเข้าเรียน</label>
-                                    <input type="text" name="enrollment_date" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                    <input type="text" name="enrollment_date" value="<?= $row['enrollment_date'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
-                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 ">
-                                    <label class="font-semibold leading-none">ชั้นที่เรียน</label>
-                                    <input type="text" name="grade_level" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
-                                </div>
+
                             </div>
                             <div class="md:flex items-center mt-4">
-                                <div class="w-full md:w-1/2 flex flex-col">
-                                    <label class="font-semibold leading-none">สถานะนักเรียน</label>
-                                    <select name="status" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200">
-                                        <option value="1">กำลังศึกษา </option>
-                                        <option value="2">ไม่ศึกษา</option>
-                                        <option value="3">สำเร็จการศึกษา</option>
-                                    </select>
+                                <div class="w-full  flex flex-col">
+                                    <label class="font-semibold leading-none">ชั้นที่เรียน</label>
+                                    <input type="text" name="grade_level" value="<?= $row['grade_level'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                                 </div>
+
                             </div>
+                            <div class="md:flex items-center mt-4">
+                                <div class="w-full  flex flex-col">
+                                    <label class="font-semibold leading-none">สถาณะนักเรียน</label>
+                                    <input type="text" name="status" value="<?= $row['status'] ?>" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                </div>
 
-
-
-                            <div class="flex items-center justify-center w-full">
+                            </div>
+                            <div class="flex items-center justify-center w-full gap-4">
                                 <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
                                     บันทึก
                                 </button>
+                                <a href="student.php" class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-red-600 rounded hover:bg-red-600 focus:ring-2 focus:ring-offset-2 focus:ring-red-700 focus:outline-none">ยกเลิก</a>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
 
-
-
-
-
-        </div>
         </div>
     </div>
-    </main>
 </body>
 
 </html>
