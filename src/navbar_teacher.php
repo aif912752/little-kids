5d91912bddd18e7d2d09ee900298487957d1e120
aif912752 <aif912752@gmail.com>
2024-10-05 21:18:18 +0700

    up

--- src/navbar_teacher.php
+++ src/navbar_teacher.php
@@ -170,7 +170,8 @@ session_start();
                         <li class="sidebar-item">
                             <a class="sidebar-link gap-3 py-2.5 my-1 text-base   flex items-center relative  rounded-md text-gray-500  w-full"
                                 href="activity.php">
-                                <i class="ti ti-article ps-2 text-2xl"></i> <span>ตารางกิจกรรม</span>
+                                <i class="ti ti-article ps-2 text-2xl"></i> 
+                                <span>ตารางกิจกรรม</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
