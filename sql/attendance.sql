06a060ae4bddcc66ecbcbc2cf706372b114010ae
warunyoo1 <151112524+warunyoo1@users.noreply.github.com>
2024-10-06 01:07:22 +0700

    Update attendance.sql

--- sql/attendance.sql
+++ sql/attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2024 at 01:47 PM
+-- Generation Time: Oct 05, 2024 at 08:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -33,8 +33,6 @@ CREATE TABLE `attendance` (
   `student_name` varchar(255) NOT NULL COMMENT 'ชื่อนักเรียน',
   `student_lastname` varchar(255) NOT NULL COMMENT 'ชื่อนักเรียน',
   `attendance_date` date NOT NULL COMMENT 'วันที่ลงเวลา',
-  `check_in_time` time NOT NULL COMMENT 'เวลาเข้า',
-  `check_out_time` time DEFAULT NULL COMMENT 'เวลาออก',
   `note` varchar(100) NOT NULL COMMENT 'หมายเหตุ',
   `status` enum('normal','absent','leave','late') DEFAULT 'normal' COMMENT 'สถานะการเข้าเรียน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -43,8 +41,8 @@ CREATE TABLE `attendance` (
 -- Dumping data for table `attendance`
 --
 
-INSERT INTO `attendance` (`attendance_id`, `student_id`, `student_name`, `student_lastname`, `attendance_date`, `check_in_time`, `check_out_time`, `note`, `status`) VALUES
-(11, 15, 'นนท์', 'กาย', '2024-10-05', '15:00:00', '20:00:00', '', 'normal');
+INSERT INTO `attendance` (`attendance_id`, `student_id`, `student_name`, `student_lastname`, `attendance_date`, `note`, `status`) VALUES
+(12, 17, 'กาย', 'กาย', '2024-10-05', '', 'absent');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +62,7 @@ ALTER TABLE `attendance`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการลงเวลา', AUTO_INCREMENT=12;
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการลงเวลา', AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
