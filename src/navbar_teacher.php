0740378b17004b9ef1dbd12bc83fee8df2e3d0c8
aif912752 <aif912752@gmail.com>
2024-10-05 16:08:28 +0700

    up

--- src/navbar_teacher.php
+++ src/navbar_teacher.php
@@ -118,7 +118,12 @@ session_start();
                                 <i class="ti ti-article ps-2 text-2xl"></i> <span>แบบประเมิน</span>
                             </a>
                         </li>
-
+                        <li class="sidebar-item">
+                            <a class="sidebar-link gap-3 py-2.5 my-1 text-base   flex items-center relative  rounded-md text-gray-500  w-full"
+                                href="profile.php">
+                                <i class="ti ti-article ps-2 text-2xl"></i> <span>โปรไฟล์</span>
+                            </a>
+                        </li>
 
                     <?php
                     } else if ($_SESSION['role'] == '4') {
