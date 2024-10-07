5c97cb66365cedf1021ebfd5f89c043cd53d5584
aif912752 <aif912752@gmail.com>
2024-10-07 22:36:36 +0700

    up

--- views/teacher/student.php
+++ views/teacher/student.php
@@ -62,7 +62,7 @@ $result = $connect->query($sql);
                                         <!-- ลิงก์เลือกห้อง -->
                                         <div class="flex justify-center">
                                             <a href="<?= "http://" . $_SERVER['HTTP_HOST'] ?>/little-kids/views/admin/room_view.php?id=<?php echo $row['room_id']; ?>"
-                                                class="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                                                class="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-900 transition duration-300">
                                                 เลือกห้องนี้
                                             </a>
                                         </div>
