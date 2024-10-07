ac1163cb58f98ed7ebd2d987f983d959f1622c13
aif912752 <aif912752@gmail.com>
2024-10-07 18:55:10 +0700

    up

--- views/teacher/profile.php
+++ views/teacher/profile.php
@@ -50,7 +50,7 @@
                                 echo '<img src="uploads/teacher/'.$teacher['img'].'" class="w-40 h-40 border-4 border-white rounded-full">';
                                 // echo '<img src="https://vojislavd.com/ta-template-demo/assets/img/profile.jpg" class="w-40 border-4 border-white rounded-full">';
                             } else {
-                                echo '<img src="https://vojislavd.com/ta-template-demo/assets/img/profile.jpg" class="w-40 border-4 border-white rounded-full">';
+                                echo '<img src="../../public/user.png" class="w-40 border-4 border-white rounded-full">';
                             }
                             ?>
                             <!-- <img src="https://vojislavd.com/ta-template-demo/assets/img/profile.jpg" class="w-40 border-4 border-white rounded-full"> -->
